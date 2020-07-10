d665380a2569f1092a6c63b206aff986e383838a
Julia Naomi <jnboeira@outlook.com>
2020-07-09 23:27:59 -0300

    http::crux test

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -47,3 +47,54 @@ impl CruxClient {
             .text()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn new() {
+        let actual = Crux::new("host", "port");
+        let expected = Crux {
+            host: String::from("host"),
+            port: String::from("port"),
+            headers: HeaderMap::new(),
+        };
+
+        assert_eq!(actual.host, expected.host);
+        assert_eq!(actual.port, expected.port);
+        assert_eq!(actual.headers, expected.headers);
+    }
+
+    #[test]
+    fn authorization() {
+        let crux = Crux::new("host", "port").with_authorization("auth");
+        let mut headers = HeaderMap::new();
+        headers.insert(AUTHORIZATION, "auth".parse().unwrap());
+
+        assert_eq!(crux.headers, headers);
+    }
+
+    #[test]
+    fn uri() {
+        let crux = Crux::new("localhost", "3000");
+
+        assert_eq!(crux.uri(), "http://localhost:3000")
+    }
+
+    #[test]
+    fn client() {
+        let mut headers = HeaderMap::new();
+        headers.insert(AUTHORIZATION, "auth".parse().unwrap());
+
+        let actual = Crux::new("localhost", "3000").with_authorization("auth").client();
+        let expected = CruxClient {
+            client: reqwest::blocking::Client::new(),
+            uri: "http://localhost:3000".to_string(),
+            headers: headers,
+        };
+
+        assert_eq!(actual.uri, expected.uri);
+        assert_eq!(actual.headers, expected.headers);
+    }
+}
