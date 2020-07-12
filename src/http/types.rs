5df9a13c2d62ae4fc8e34395ef2b011ce5f6ebf5
Otávio Pace <otaviopp8@gmail.com>
2020-07-11 23:39:45 -0300

    StateResponse: change deserialize from HashMap to regex with grok
    Co-authored-by: Julia Boeira <julia.boeira@nubank.com.br>

--- src/http/types.rs
+++ src/http/types.rs
@@ -2,7 +2,7 @@ use edn_rs::{
     ser_struct,
     serialize::Serialize
 };
-use std::collections::HashMap;
+use grok::Grok;
 
 ser_struct!{
     #[derive(Debug, PartialEq)]
@@ -18,19 +18,62 @@ ser_struct!{
     }
 }
 
-impl StateResponse {
-    pub fn deserialize(resp: String) -> StateResponse {
-        let mut hashmap = HashMap::new();
-        let data = resp.replace("{","").replace("}","");
-        let key_val = data.split(", ")
-            .map(|v| v.split(" ").collect::<Vec<&str>>())
-            .map(|v| (v[0], v[1]))
-            .collect::<Vec<(&str, &str)>>();
-        for (key, val) in key_val.iter() {
-            hashmap.insert(String::from(*key), String::from(*val));
-        }
+#[derive(Debug)]
+pub enum Error {
+    ParseError(String),
+    RequestError(String),
+}
 
-        hashmap.into()
+impl StateResponse {
+    pub fn deserialize(edn: String) -> Result<StateResponse, Error> {
+        let mut grok = Grok::default();
+
+        grok.insert_definition("ALL", r"(?=.*:crux.tx-log/consumer-state %{TX_CONSUMER_STATE})(?=.*:crux.index/index-version %{VERSION})(?=.*:crux.doc-log/consumer-state %{DOC_CONSUMER_STATE})(?=.*:crux.kv/kv-store %{KV_STORE})(?=.*:crux.kv/estimate-num-keys %{NUM_KEYS})(?=.*:crux.kv/size %{SIZE})");
+        grok.insert_definition("VERSION", r"[0-9]+");
+        grok.insert_definition("KV_STORE", r#"\"(\w+[\./]*)+\""#);
+        grok.insert_definition("DOC_CONSUMER_STATE", r"{.*?}|nil");
+        grok.insert_definition("TX_CONSUMER_STATE", r"{.*?}|nil");
+        grok.insert_definition("NUM_KEYS", r"[0-9]+");
+        grok.insert_definition("SIZE", r"[0-9]+");
+
+        let pattern = grok.compile("%{ALL}", false)
+            .expect("Error while compiling!");
+
+        match pattern.match_against(&edn) {
+            Some(m) => {
+                Ok(StateResponse {
+                    index__index_version: m
+                        .get("VERSION")
+                        .ok_or_else(|| Error::ParseError(":crux.index/index-version not found".to_string()))?
+                        .parse::<usize>()
+                        .map_err(|_| Error::ParseError(":crux.index/index-version not a number".to_string()))?,
+                    doc_log__consumer_state: m
+                        .get("DOC_CONSUMER_STATE")
+                        .map(ToString::to_string)
+                        .and_then(nullable_str),
+                    tx_log__consumer_state: m
+                        .get("TX_CONSUMER_STATE")
+                        .map(ToString::to_string)
+                        .and_then(nullable_str),
+                    kv__kv_store: m
+                        .get("KV_STORE")
+                        .ok_or_else(|| Error::ParseError(":crux.kv/kv-store not found".to_string()))?
+                        .to_string()
+                        .replace("\"", ""),
+                    kv__estimate_num_keys: m
+                        .get("NUM_KEYS")
+                        .ok_or_else(|| Error::ParseError(":crux.kv/estimate-num-keys not found".to_string()))?
+                        .parse::<usize>()
+                        .map_err(|_| Error::ParseError(":crux.kv/estimate-num-keys not a number".to_string()))?,
+                    kv__size: m
+                        .get("SIZE")
+                        .ok_or_else(|| Error::ParseError(":crux.kv/size not found".to_string()))?
+                        .parse::<usize>()
+                        .map_err(|_| Error::ParseError(":crux.kv/size not a number".to_string()))?,
+                })
+            },
+            None => Err(Error::ParseError("Failed to deserialize StateResponse".to_string())),
+        }
     }
 
     #[cfg(test)]
@@ -42,19 +85,6 @@ impl StateResponse {
             kv__kv_store: String::from("crux.kv.rocksdb.RocksKv"),
             kv__estimate_num_keys: 34usize,
             kv__size: 88489usize,
-        }
-    }
-}
-
-impl From<HashMap<String,String>> for StateResponse {
-    fn from(hm: HashMap<String,String>) -> Self {
-        StateResponse {
-            index__index_version: hm[":crux.index/index-version"].parse::<usize>().unwrap_or(0usize),
-            doc_log__consumer_state: nullable_str(hm[":crux.doc-log/consumer-state"].clone()),
-            tx_log__consumer_state:  nullable_str(hm[":crux.tx-log/consumer-state"].clone()),
-            kv__kv_store: hm[":crux.kv/kv-store"].clone().replace("\"", ""),
-            kv__estimate_num_keys: hm[":crux.kv/estimate-num-keys"].parse::<usize>().unwrap_or(0usize),
-            kv__size: hm[":crux.kv/size"].parse::<usize>().unwrap_or(0usize),
         }
     }
 }
